e085de8f271f9f7bb8167cdb88f6c229f0706614
LukaDrozdek <Luka04111993@gmail.com>
2022-09-04 01:19:32 +0200

    Add project files.

---
+++ KING_Test/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KING_Test.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
